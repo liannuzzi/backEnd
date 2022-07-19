695718ddae194af68246c9dc1e157c40c28f9f71
Lucas <iannuzzilucas@hotmail.com>
2022-07-19 19:45:18 -0300

    Re-entrega desafio

--- server.js
+++ server.js
@@ -20,25 +20,23 @@ class Contenedor{
         const productos = fs.readFileSync(this.filePath,'utf-8')
         const arrayProductos=JSON.parse(productos)
         const maxId= Math.max.apply(Math,arrayProductos.map(item=>item.id))
-        const minId= Math.min.apply(Math,arrayProductos.map(item=>item.id))
+        const randomNumber=parseInt(Math.ceil( Math.random() * maxId))
+        const resultadoBusqueda = arrayProductos.find(product=>product.id===parseInt(randomNumber))
 
-        const max = Math.floor(maxId);
-        const min = Math.ceil(minId);
-        
-        const randomNumber=parseInt(Math.floor(Math.random() * (max - min + 1) + min));
-
-        return randomNumber
+        return resultadoBusqueda
     }
 
     getAll(){
         return fs.promises.readFile(this.filePath,'utf-8')
         .then( productos =>{
-            const arrayProductos= JSON.parse(productos);
+            const arrayProductos= JSON.parse(productos)
             return arrayProductos
             
         })
         .catch ( error => {
-            console.log('No se puede leer archivo', error)
+            
+            const msjError = `No se puede leer el archivo ${error}`
+            return msjError
         })
 
     }
@@ -63,11 +61,8 @@ app.get('/productos',(req,res)=>{
     res.send(allProducts)
 })
 
-let randomProduct = productos.getRandomProduct()
-console.log(randomProduct)
-
 app.get('/productoRandom',(req,res)=>{
    
-    res.send({productoRandom:randomProduct})
+    res.send({productoRandom:productos.getRandomProduct()})
 })
 
